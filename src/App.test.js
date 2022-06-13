f6fcf75419e34cab71291197793a6077a661965d
Francisco Melo <franciscomelo@oi.net.br>
2022-06-13 17:03:37 -0300

    React Testing Library

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, getByLabelText, getByTestId, getByText, render, screen } from '@testing-library/react';
 import App, { calcularNovoSaldo } from './App';
 
 describe('Componente Principal', () => {
@@ -18,13 +18,29 @@ describe('Componente Principal', () => {
         });
     });
     describe('Quando realizo uma operação', () => {
-        it('Saque o saldo diminui', () => {
-            const valores = {
-                transacao: 'saque',
-                valor: 50,
-            };
-            const novoSaldo = calcularNovoSaldo(valores, 150);
-            expect(novoSaldo).toBe(100);
+        // it('Saque o saldo diminui', () => {
+        //     const valores = {
+        //         transacao: 'saque',
+        //         valor: 50,
+        //     };
+        //     const novoSaldo = calcularNovoSaldo(valores, 150);
+        //     expect(novoSaldo).toBe(100);
+        // });
+        it.only('Clique no botão Saque o saldo diminui', () => {
+            const { getByText, getByLabelText, getByTestId } = render(<App />);
+
+            const saldo = getByText('R$ 1000');
+            const transacao = getByLabelText('Saque');
+            const valor = getByTestId('valor');
+            const botaoOperacao = getByText('Realizar operação');
+
+            expect(saldo.textContent).toBe('R$ 1000');
+
+            fireEvent.click(transacao, { target: { value: 'saque' } });
+            fireEvent.change(valor, { target: { value: 10 } });
+            fireEvent.click(botaoOperacao);
+
+            expect(saldo.textContent).toBe('R$ 990');
         });
     });
 });
